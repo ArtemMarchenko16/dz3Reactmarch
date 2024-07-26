4dafed533f2e55a1b56e7105fd5a4247501054b9
ArtemMarchenko16 <110462384+ArtemMarchenko16@users.noreply.github.com>
2024-07-26 15:59:10 +0300

    dz33

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React, {FC, useState} from 'react';
+import './App.css';
+import {useToggle} from './components/hook';
+import {usePrevious} from "./components/hook2";
+
+const App:FC = () => {
+
+
+  const[num,setNum] = useState(0);
+  let [currentValue,prevValue] = usePrevious<number>(num);
+
+  let{changeHandler,value}= useToggle(false);
+  return (
+      <>
+        <h3>{value.toString()}</h3>
+        <button onClick={() => {
+          console.log(value);
+          changeHandler();
+        }}>change
+        </button>
+
+        <hr/>
+        <h4>Current value:{currentValue}</h4>
+        <h4>Previous value:{prevValue}</h4>
+        <button onClick={() => {
+          setNum(currentValue += 5)
+        }}>plus 10
+        </button>
+
+      </>
+  );
+}
+
+export default App;
